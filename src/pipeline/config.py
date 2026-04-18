5b2dce4bdaa24658ca148f52eba35eab65c8fe7b
Salma Muhammed Entsar <salmamohed207@gmail.com>
2026-04-18 14:45:07 +0200

    updating config file for pth error

--- src/pipeline/config.py
+++ src/pipeline/config.py
@@ -4,5 +4,4 @@ test_transformed_path = "data/transformed/test_transformed.csv"
 
 train_cleaned_path = "data/clean/train_cleaned.csv"
 val_cleaned_path = "data/clean/val_cleaned.csv"
-test_cleaned_path ="data/clean/test_cleaned.csv"
-
+test_cleaned_path = "data/clean/test_cleaned.csv"
