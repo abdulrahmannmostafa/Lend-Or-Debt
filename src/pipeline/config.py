3709dad4f19d05c15f6cab9d61e1b01053c50d34
Salma Muhammed Entsar <salmamohed207@gmail.com>
2026-04-18 14:35:53 +0200

    updating config file for pth error

--- src/pipeline/config.py
+++ src/pipeline/config.py
@@ -1,13 +1,8 @@
-train_transformed_path = "D:/CMP4rd/second term/Data Science/Lend-Or-Debt/data/transformed/train_transformed.csv"
-val_transformed_path = "D:/CMP4rd/second term/Data Science/Lend-Or-Debt/data/transformed/val_transformed.csv"
-test_transformed_path = "D:/CMP4rd/second term/Data Science/Lend-Or-Debt/data/transformed/test_transformed.csv"
+train_transformed_path = "data/transformed/train_transformed.csv"
+val_transformed_path = "data/transformed/val_transformed.csv"
+test_transformed_path = "data/transformed/test_transformed.csv"
 
-train_cleaned_path = (
-    "D:/CMP4rd/second term/Data Science/Lend-Or-Debt/data/clean/train_cleaned.csv"
-)
-val_cleaned_path = (
-    "D:/CMP4rd/second term/Data Science/Lend-Or-Debt/data/clean/val_cleaned.csv"
-)
-test_cleaned_path = (
-    "D:/CMP4rd/second term/Data Science/Lend-Or-Debt/data/clean/test_cleaned.csv"
-)
+train_cleaned_path = "data/clean/train_cleaned.csv"
+val_cleaned_path = "data/clean/val_cleaned.csv"
+test_cleaned_path ="data/clean/test_cleaned.csv"
+
